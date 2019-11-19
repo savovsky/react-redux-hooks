e2b4463a0119e79ac789b8869fc2296659073dca
Miroslav Savovski <miroslav.savovski@scalefocus.com>
2019-11-19 15:47:44 +0200

    Init file structure - added.

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PostList extends React.Component {
+
+  render() {
+    return (
+      <div>PostList component</div>
+    );
+  }
+
+}
+
+export default PostList;
