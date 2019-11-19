981d347f1466a6cf1bad063e1e5ae84ca43f8a95
Miroslav Savovski <miroslav.savovski@scalefocus.com>
2019-11-19 17:25:40 +0200

    Redux Thunk and async-await in action creator - added.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,21 @@
-// import jsonPlaceholder from '../apis/jsonPlaceholder';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
-    }
-}
+// Action creators
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('posts');
+
+  dispatch({ type: 'FEYCH_POSTS', payload: response });
+};
+
+
+// REMIND Redux Thunk allows us to return a function OR object from a Action creator.
+// Stephen 165 'Behind the Scenes of Redux Thunk'
+
+// export const fetchPosts = () => {
+//   return async function(dispatch, getState) {
+//     const response = await jsonPlaceholder.get('posts');
+
+//     dispatch({ type: 'FEYCH_POSTS', payload: response });
+//   }
+// };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 const Application = () => {
   return (
     <Provider store={store}>
