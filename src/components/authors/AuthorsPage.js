ec35cc28ecfa5ee9822bed4f48a25781129dc9b2
Miroslav Savovski <miroslav.savovski@scalefocus.com>
2019-12-05 14:02:09 +0200

    columns-grid - added.

---
+++ src/components/authors/AuthorsPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const AuthorsPage = () => (
+  <div className="columns-container">
+    <div className="column-wrapper">
+      <div className="column">1</div>
+    </div>
+    <div className="column-wrapper">
+      <div className="column">2</div>
+    </div>
+    <div className="column-wrapper">
+      <div className="column">3</div>
+    </div>
+  </div>
+);
+
+export default AuthorsPage;
