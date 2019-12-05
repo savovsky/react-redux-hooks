6239a7c4572e98de9d61577c2e2f4e6990badbdb
Miroslav Savovski <miroslav.savovski@scalefocus.com>
2019-12-05 18:24:21 +0200

    columns-grid - style.

--- src/components/authors/AuthorsPage.js
+++ src/components/authors/AuthorsPage.js
@@ -4,13 +4,39 @@ import React from 'react';
 const AuthorsPage = () => (
   <div className="columns-container">
     <div className="column-wrapper">
-      <div className="column">1</div>
+      <div className="column">
+        <div className="card p-20 m-10">
+          <h3 className="card-header m-b-15">123</h3>
+          <div className="card-body m-b-15">jnerpfnerfoj</div>
+          <div className="card-footer">irfpqieurfnq0e</div>
+        </div>
+        <div className="card p-20 m-10">
+          <h3 className="card-header m-b-15">123</h3>
+          <div className="card-body m-b-15">jnerpfnerfoj</div>
+          <div className="card-footer">irfpqieurfnq0e</div>
+        </div>
+        <div className="card p-20 m-10">
+          <h3 className="card-header m-b-15">123</h3>
+          <div className="card-body m-b-15">jnerpfnerfoj</div>
+          <div className="card-footer">irfpqieurfnq0e</div>
+        </div>
+      </div>
     </div>
     <div className="column-wrapper">
-      <div className="column">2</div>
+      <div className="column">
+        <div className="card p-20 m-10">
+          <h3 className="card-header m-b-15">123</h3>
+        </div>
+        <div className="card p-20 m-10">
+          <div className="card-body m-b-15">jnerpfnerfoj</div>
+        </div>
+        <div className="card p-20 m-10">
+          <div className="card-footer m-b-15">jnerpfnerfoj</div>
+        </div>
+      </div>
     </div>
     <div className="column-wrapper">
-      <div className="column">3</div>
+      <div className="column"></div>
     </div>
   </div>
 );
